4cfca498735cc612a3b648122c011b4df7407a5e
Blackmaskexe <82077507+blackmaskexe@users.noreply.github.com>
2025-11-03 21:01:14 -0600

    Initial Commit

---
+++ goal-folio/goal_folioApp.swift
@@ -0,0 +1,17 @@
+//
+//  goal_folioApp.swift
+//  goal-folio
+//
+//  Created by Pratham S on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct goal_folioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
